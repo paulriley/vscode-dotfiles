f21bbb1d92ee76adf16b4a5d2d37bd62984a98ab
Paul Riley <paul@wsnhsolutions.com>
2020-05-04 12:56:48 +0100

    Make zsh the default, so the devcontainer doesn't have to

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "Installing oh-my-zsh"
+
+echo "Installing and customizing oh-my-zsh"
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh || true
+
 echo "Customizing oh-my-zsh"
 cp .zshrc ~/.zshrc
+
+chsh -s $(which zsh)
