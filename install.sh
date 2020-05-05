5cc71cc40e094ea432800ab04457c651935eba8d
Paul Riley <paul@wsnhsolutions.com>
2020-05-05 15:02:57 +0000

    Use https

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing and customizing oh-my-zsh"
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh || true
 
 echo "Installing my own themes"
-git clone git@github.com:paulriley/zsh-themes.git
+git clone https://github.com/paulriley/zsh-themes.git
 cp zsh-themes/*.zsh-theme ~/.oh-my-zsh/themes
 rm -rf zsh-themes
 
