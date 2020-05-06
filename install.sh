bb445c850b0790cf3fafbb250da509143c383b7f
Paul Riley <paul@wsnhsolutions.com>
2020-05-06 21:31:28 +0100

    Fix: line continuations

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "Installing and customizing oh-my-zsh"
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh || true
 
 echo "Installing my own themes"
-git clone https://github.com/paulriley/zsh-themes.git /tmp/zsh-themes/
-&& cp /tmp/zsh-themes/*.zsh-theme ~/.oh-my-zsh/themes
+git clone https://github.com/paulriley/zsh-themes.git /tmp/zsh-themes/ \
+&& cp /tmp/zsh-themes/*.zsh-theme ~/.oh-my-zsh/themes \
 && rm -rf zsh-themes
 
 echo "Customizing oh-my-zsh"
