94a042f574562477d3b100434d8ec3021ae35810
Paul Riley <paul@wsnhsolutions.com>
2020-05-06 17:56:26 +0100

    Fix: clone to tmp

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing and customizing oh-my-zsh"
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh || true
 
 echo "Installing my own themes"
-git clone https://github.com/paulriley/zsh-themes.git 
+git clone https://github.com/paulriley/zsh-themes.git /tmp/zsh-themes/
 cp /tmp/zsh-themes/*.zsh-theme ~/.oh-my-zsh/themes
 rm -rf zsh-themes
 
